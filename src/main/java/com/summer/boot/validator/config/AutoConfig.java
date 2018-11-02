fe539395dd740e809760df052b0aed6be80150c7
yalunwang <yalunwang123@163.com>
2018-11-02 18:57:22 +0800

    初始化项目

---
+++ src/main/java/com/summer/boot/validator/config/AutoConfig.java
@@ -0,0 +1,8 @@
+package com.summer.boot.validator.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.summer.boot.validator")
+public class AutoConfig {
+}
